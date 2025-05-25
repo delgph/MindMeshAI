36e0bb54b247fa33a5917a407923e6f4ca967687
Jingyi Zhao <zhao.elton@gmail.com>
2025-05-24 21:41:10 -0700

    - fix: fix api integration

--- agents/crew_format/main.py
+++ agents/crew_format/main.py
@@ -1,18 +1,15 @@
-import sys
-from crew import Sentiment
-
-def run():
-    """
-    Run the crew.
-    """
-    inputs ={"text": "Im feeling depressed and Im not happy",
-            "user":"Diana",
-             "days":"12 days"
-            }
-
-    Sentiment().crew().kickoff(inputs=inputs)
-run()
-
+# import sys
+#
+# from agents.crew_format.crew import Sentiment
+#
+#
+# def run(query):
+#     """
+#     Run the crew.
+#     """
+#
+#
+#
 
 
 
